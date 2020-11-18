bb7ac8823616d5d4e967e7e0f247701dcf145ee8
Raphael <raphaelalbino_@hotmail.com>
2020-11-18 02:58:59 -0300

    CheckPoint Implemented

---
+++ Assets/Scripts/CheckPointTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+    public class CheckPointTrigger : MonoBehaviour
+    {
+        internal Collider2D _collider; 
+
+        void Awake()
+        {
+            _collider = GetComponent<Collider2D>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            var player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                _collider.enabled = false;
+                var ev = Schedule<UpdateSpawnPoint>();
+                ev.newPos = transform;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UpdateSpawnPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Platformer.Core;
+using Platformer.Model;
+using UnityEngine;
+
+namespace Platformer.Gameplay
+{
+    /// <summary>
+    /// Fired when the player has died.
+    /// </summary>
+    /// <typeparam name="PlayerDeath"></typeparam>
+    public class UpdateSpawnPoint : Simulation.Event<UpdateSpawnPoint>
+    {
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+        public Transform newPos;
+        public override void Execute()
+        {
+            model.spawnPoint = newPos;
+
+        }
+    }
+}
