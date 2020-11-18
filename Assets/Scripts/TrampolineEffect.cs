c32b413cb4de1645c413e1217c062749ab79bb36
Raphael <raphaelalbino_@hotmail.com>
2020-11-18 03:50:43 -0300

    Trampoline Implemented (v1)

---
+++ Assets/Scripts/TrampolineEffect.cs
@@ -0,0 +1,32 @@
+﻿using Platformer.Core;
+using Platformer.Mechanics;
+using Platformer.Model;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Gameplay
+{
+
+    /// <summary>
+    /// Fired when a Player collides with an Enemy.
+    /// </summary>
+    /// <typeparam name="EnemyCollision"></typeparam>
+    public class TrampolineEffect : Simulation.Event<TrampolineEffect>
+    {
+        public TrampolineTrigger trampoline;
+        public PlayerController player;
+
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        public override void Execute()
+        {
+            var willJump = player.Bounds.center.y >= trampoline.Bounds.max.y;
+
+            if (willJump)
+            {
+                var enemyHealth = trampoline.GetComponent<Health>();
+                player.Bounce(7);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TrampolineTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Platformer.Gameplay;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Mechanics
+{
+
+    public class TrampolineTrigger : MonoBehaviour
+    {
+        internal Collider2D _collider;
+        public Bounds Bounds => _collider.bounds;
+
+        void Awake()
+        {
+            _collider = GetComponent<Collider2D>();
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            var player = collision.gameObject.GetComponent<PlayerController>();
+            if (player != null)
+            {
+                var ev = Schedule<TrampolineEffect>();
+                ev.player = player;
+                ev.trampoline = this;
+            }
+        }
+    }
+}
